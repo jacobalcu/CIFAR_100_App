c7eb47debce5a62ead36c42504873922bffe56a4
jalcumb <101678531+jacobalcu@users.noreply.github.com>
2025-12-16 18:02:00 -0500

    added transform_image()

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,12 @@
 # Includes helpter function (e.g. image transformations)
+from torchvision import transforms
+
+
+def transform_image(image):
+    stats = ((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
+
+    test_transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize(*stats)]
+    )
+
+    return test_transform(image)
