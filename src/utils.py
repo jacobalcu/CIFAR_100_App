6eab89d275b6481da08e5946ad69624252c63ae3
jalcumb <101678531+jacobalcu@users.noreply.github.com>
2026-01-10 16:10:07 -0500

    added resize to utils. Should fix innacuracy

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,11 @@ def transform_image(image):
     stats = ((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
 
     test_transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize(*stats)]
+        [
+            transforms.Resize((32, 32)),
+            transforms.ToTensor(),
+            transforms.Normalize(*stats),
+        ]
     )
 
     return test_transform(image)
